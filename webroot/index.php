69029c3a78c3817b6564e77420e49fc2412a34a0
SLC <uc@slokvenko.com>
2018-09-24 22:26:39 +0300

    added frontend libraries

--- webroot/index.php
+++ webroot/index.php
@@ -74,107 +74,144 @@ if (!empty($_POST['isset_data'])) {
     <title>Jira Worklogs</title>
     <link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="node_modules/datatables.net-dt/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="node_modules/datatables.net-buttons-dt/css/buttons.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="node_modules/datatables.net-buttons-bs4/css/buttons.bootstrap4.css">
+
+    <link rel="stylesheet" type="text/css" href="node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" type="text/css" href="node_modules/select2/dist/css/select2.css">
+    <link rel="stylesheet" type="text/css" href="node_modules/select2-bootstrap-theme/dist/select2-bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" type="text/css" href="node_modules/datatables.net-responsive-bs4/css/responsive.bootstrap4.css">
+
+
     <script type="text/javascript" charset="utf8" src="node_modules/jquery/dist/jquery.js"></script>
     <script type="text/javascript" charset="utf8" src="node_modules/datatables.net/js/jquery.dataTables.js"></script>
-    <script type="text/javascript" charset="utf8"
-            src="node_modules/datatables.net-buttons/js/dataTables.buttons.js"></script>
-    <script type="text/javascript" charset="utf8"
-            src="node_modules/datatables.net-buttons/js/buttons.colVis.js"></script>
-    <script type="text/javascript" charset="utf8"
-            src="node_modules/datatables.net-buttons/js/buttons.flash.js"></script>
+    <script type="text/javascript" charset="utf8" src="node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js"></script>
+
+    <script type="text/javascript" charset="utf8" src="node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
+    <script type="text/javascript" charset="utf8" src="node_modules/select2/dist/js/select2.js"></script>
+
+    <script type="text/javascript" charset="utf8" src="node_modules/jszip/dist/jszip.js"></script>
+    <script type="text/javascript" charset="utf8" src="node_modules/pdfmake/build/pdfmake.js"></script>
+    <script type="text/javascript" charset="utf8" src="node_modules/pdfmake/build/vfs_fonts.js"></script>
+    <script type="text/javascript" charset="utf8" src="node_modules/datatables.net-buttons/js/dataTables.buttons.js"></script>
+<!--    <script type="text/javascript" charset="utf8"-->
+<!--            src="node_modules/datatables.net-buttons/js/buttons.colVis.js"></script>-->
+<!--    <script type="text/javascript" charset="utf8"-->
+<!--            src="node_modules/datatables.net-buttons/js/buttons.flash.js"></script>-->
+    <script type="text/javascript" charset="utf8" src="node_modules/datatables.net-buttons-bs4/js/buttons.bootstrap4.js"></script>
     <script type="text/javascript" charset="utf8"
             src="node_modules/datatables.net-buttons/js/buttons.print.js"></script>
     <script type="text/javascript" charset="utf8"
             src="node_modules/datatables.net-buttons/js/buttons.html5.js"></script>
+
+    <script type="text/javascript" charset="utf8" src="node_modules/datatables.net-responsive/js/dataTables.responsive.js"></script>
+    <script type="text/javascript" charset="utf8" src="node_modules/datatables.net-responsive-bs4/js/responsive.bootstrap4.js"></script>
 </head>
 <body>
-<h1>Jira Worklogs</h1>
-<form method="post">
-    <select name="project">
-        <?php
-        foreach ($projects as $project) {
-            echo '<option value="' . $project->key . '" ' . (($project->key == $_POST['project']) ? ' selected="selected" ' : '') . ' >' . $project->name . ' (' . $project->key . ')</option>';
-        }
-        ?>
-    </select>
-    <br>
-    <br>
-    <?php if (!empty($users)) { ?>
-        <select name="user">
-            <option value="">All</option>
-            <?php
-            foreach ($users as $user) {
-                echo '<option value="' . $user->key . '" ' . (($user->key == $_POST['user']) ? ' selected="selected" ' : '') . ' >' . $user->displayName . ' &lt;' . $user->emailAddress . '&gt;</option>';
-            }
-            ?>
-        </select>
-        <br>
-        <br>
-        from
-        <input type="text" name="startDate" value="<?php echo $startDate ?>">
-        to
-        <input type="text" name="endDate" value="<?php echo $endDate ?>">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
 
-        <input type="hidden" name="isset_data" value="1">
-        <br><br>
-        <button type="submit">Calculate</button>
-
-
-    <?php } else { ?>
-        <br><br>
-        <button type="submit">Set Project</button>
-    <?php } ?>
-</form>
-<br><br><br>
-<?php if (!empty($_POST['isset_data'])) { ?>
-
-    <table id="jiraDataTable" cellpadding="1" cellspacing="0" border="1">
-        <thead>
-        <tr>
-            <th>Issue</th>
-            <th>User</th>
-            <th>Comment</th>
-            <th>Date</th>
-            <th>Time</th>
-            <th>Time in seconds</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        $commonTime = 0;
-        foreach ($resultIssues as $issue) {
+            <h1>Jira Worklogs</h1>
+            <form method="post">
+                <div class="form-group">
+                    <select name="project" class="form-control select2">
+                        <?php
+                        foreach ($projects as $project) {
+                            echo '<option value="' . $project->key . '" ' . (($project->key == $_POST['project']) ? ' selected="selected" ' : '') . ' >' . $project->name . ' (' . $project->key . ')</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
+                <?php if (!empty($users)) { ?>
+                    <div class="form-group">
+                        <select name="user" class="form-control select2">
+                            <option value="">All</option>
+                            <?php
+                            foreach ($users as $user) {
+                                echo '<option value="' . $user->key . '" ' . (($user->key == $_POST['user']) ? ' selected="selected" ' : '') . ' >' . $user->displayName . ' &lt;' . $user->emailAddress . '&gt;</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-2">
+                                from
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" name="startDate" class="form-control datepicker datepicker-inline" value="<?php echo $startDate ?>">
+                            </div>
+                            <div class="col-md-2">
+                                to
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" name="endDate" class="form-control datepicker datepicker-inline" value="<?php echo $endDate ?>">
+                            </div>
+                        </div>
+                    </div>
+                <div class="form-group">
+                    <input type="hidden" name="isset_data" value="1">
+                    <button type="submit" class="btn btn-primary">Calculate</button>
+                </div>
+                <?php } else { ?>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success">Set Project</button>
+                </div>
+                <?php } ?>
+            </form>
+            <br><br><br>
+            <?php if (!empty($_POST['isset_data'])) { ?>
+                <table id="jiraDataTable" class="table dataTables_wrapper dt-bootstrap4">
+                    <thead>
+                    <tr>
+                        <th>Issue</th>
+                        <th>User</th>
+                        <th>Comment</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Time in seconds</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $commonTime = 0;
+                    foreach ($resultIssues as $issue) {
 //                echo '<tr><td colspan="4">';
 //                echo $issue->key;
 //                echo '</td></tr>';
-            foreach ($issue->fields->worklog->worklogs as $worklog) {
-                $workLogDate = (new \DateTime($worklog->started))->format('Y-m-d');
+                        foreach ($issue->fields->worklog->worklogs as $worklog) {
+                            $workLogDate = (new \DateTime($worklog->started))->format('Y-m-d');
 
-                if ($workLogDate >= $startDate && $workLogDate <= $endDate && (empty($userKey) || $userKey == $worklog->author->key || $userKey == $worklog->author->name)) {
-                    $commonTime += $worklog->timeSpentSeconds;
-                    echo '<tr>';
-                    //echo '<td><a href="' . $issue->self . '">' . $issue->key . '</a></td>';
-                    echo '<td>' . $issue->key . '</td>';
-                    //echo '<td><a href="' . $worklog->author->self . '">' . $worklog->author->displayName . '</a></td>';
-                    echo '<td>' . $worklog->author->displayName . '</td>';
-                    //echo '<td><a href="' . $worklog->self . '">' . $worklog->comment . '</a></td>';
-                    echo '<td>' . $worklog->comment . '</td>';
-                    echo '<td>' . (new \DateTime($worklog->started))->format('Y-m-d') . '</td>';
-                    echo '<td>' . $worklog->timeSpent . '</td>';
-                    echo '<td>' . $worklog->timeSpentSeconds . '</td>';
-                    echo '</tr>';
-                }
+                            if ($workLogDate >= $startDate && $workLogDate <= $endDate && (empty($userKey) || $userKey == $worklog->author->key || $userKey == $worklog->author->name)) {
+                                $commonTime += $worklog->timeSpentSeconds;
+                                echo '<tr>';
+                                //echo '<td><a href="' . $issue->self . '">' . $issue->key . '</a></td>';
+                                echo '<td>' . $issue->key . '</td>';
+                                //echo '<td><a href="' . $worklog->author->self . '">' . $worklog->author->displayName . '</a></td>';
+                                echo '<td>' . $worklog->author->displayName . '</td>';
+                                //echo '<td><a href="' . $worklog->self . '">' . $worklog->comment . '</a></td>';
+                                echo '<td>' . $worklog->comment . '</td>';
+                                echo '<td>' . (new \DateTime($worklog->started))->format('Y-m-d') . '</td>';
+                                echo '<td>' . $worklog->timeSpent . '</td>';
+                                echo '<td>' . $worklog->timeSpentSeconds . '</td>';
+                                echo '</tr>';
+                            }
 
-            }
-        }
-        ?>
-        </tbody>
+                        }
+                    }
+                    ?>
+                    </tbody>
 
-    </table>
+                </table>
 
-    <h2>Common time: <?php echo round($commonTime / 3600, 2); ?></h2>
+                <h2>Common time: <?php echo round($commonTime / 3600, 2); ?></h2>
 
-<?php } ?>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+
 <script type="text/javascript">
   $(document).ready(function () {
     $('#jiraDataTable').DataTable({
@@ -183,6 +220,14 @@ if (!empty($_POST['isset_data'])) {
         'copy', 'csv', 'excel', 'pdf', 'print'
       ],
       "paging": false,
+    });
+
+    $('.datepicker').datepicker({
+      format: 'yyyy-mm-dd',
+    });
+
+    $('.select2').select2({
+      theme: "bootstrap"
     });
   });
 </script>
